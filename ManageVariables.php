b3580d04e211a5c4ef26ff53ddcc9b7af0adaf4e
koop <koop@bd.pl>
2017-05-30 21:25:45 +0200

    first commit. v 1.0.0

---
+++ ManageVariables.php
@@ -0,0 +1,36 @@
+<?
+class ManageVariables
+{
+    private static $mem = array();
+
+    public static function set($namevariables, $value)
+    {
+        self::$mem[$namevariables] = $value;
+    }
+    public static function get($namevariables)
+    {
+        return self::$mem[$namevariables];
+    }
+    public static function exist($namevariables)
+    {
+        return isset(self::$mem[$namevariables]);
+    }
+    public static function getALL()
+    {
+        print_r(self::$mem);
+    }
+}
+
+/**
+ * Set Variable
+ */
+function _set($name, $value){
+    Config::set(strtolower($name), $value);
+}
+
+/**
+ * Get Variable
+ */
+function _get($name){
+    return Config::get(strtolower($name));
+}
